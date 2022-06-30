3fc27fbd2da6a58c42adc18f5349887d443404bb
itasworkshop <itasworkshop@gmail.com>
2022-06-30 09:01:43 +0530

    30 June 1

--- notes/concepts-sboot/src/main/java/com/mysite/main/MyUserController.java
+++ notes/concepts-sboot/src/main/java/com/mysite/main/MyUserController.java
@@ -1,8 +1,14 @@
 package com.mysite.main;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +25,35 @@ public class MyUserController {
 	List<User> getAll(){
 		return userRepository.findAll();
 	}
+	
+	@GetMapping("/users/{id}")
+	Optional<User> getUserById(@PathVariable Long id){
+		return userRepository.findById(id);
+	}
+	
+	//sign up form operation
+	@PostMapping("/users")
+	User newUser(@RequestBody User user){
+		return userRepository.save(user);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	void deleteUserById(@PathVariable Long id){
+		userRepository.deleteById(id);
+	}
+	
+	@PutMapping("/users/{id}")
+	User updateUser(@PathVariable Long id, @RequestBody User newuser){
+		return userRepository.findById(id).map(
+				user -> {
+					user.setName(newuser.getName());
+					return userRepository.save(user);
+				}).orElseGet(() -> {
+					newuser.setId(id);
+					return userRepository.save(newuser);
+				});
+	}
+	
 
+	
 }
